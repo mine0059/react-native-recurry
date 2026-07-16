21b996031c5ed4e67a62c1c44f945c3535c7450a
mine0059 <onojefemuemine@gmail.com>
2026-07-16 10:39:30 -0700

    implements fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,7 +10,7 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className='flex-1 bg-background p-5'>
-      <Text className="text-xl font-bold text-success">Welcome to Nativewind!</Text>
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
       <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
         Go to Onboarding
       </Link>
@@ -19,23 +19,6 @@ export default function App() {
       </Link>
       <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">
         Go to Signup
-      </Link>
-
-      <Link
-        href={{
-          pathname: '/subscriptions/[id]',
-          params: { id: 'spotify' },
-        }}
-      >
-        Spotify Subscription
-      </Link>
-      <Link
-        href={{
-          pathname: '/subscriptions/[id]',
-          params: { id: 'claude' },
-        }}
-      >
-        Claude Max Subscription
       </Link>
     </SafeAreaView>
   );
